90ad5671fa0f11c230d3baf248ddb657f8dc4a6d
lakshmikant2007 <lakshmikantkumar22@gmail.com>
2026-01-15 09:25:27 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    printf(" Enter two nubers ");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf(" sum of %d and %d is %d ",a,b,c);
+    getch();
+}
