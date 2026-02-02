f9414599064055d0da9b27fa169106aded80f81c
lakshmikant2007 <lakshmikantkumar22@gmail.com>
2026-02-02 09:42:51 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int x;
+    printf("Enter the number");
+    scanf("%d",&x);
+    if(x>0)
+        printf("positive");
+    else
+        printf("non positive");
+    getch();
+}
