0ec91f7e94acd93fd306e95a5ed6bb7517a75a9a
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-02-03 08:00:47 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <conio.h>
+#include<stdio.h>
+#include<conio.h>
 int main()
 {
-    int x;
-    printf("Enter a number");
-    scanf("%d",&x);
-    if(x %5 == 0)
-        printf("Divisible by 5");
-    else
-        printf("not divisible by 5");
+    int a=20,b=30;
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    printf("%d,%d",a,b);
     getch();
 }
