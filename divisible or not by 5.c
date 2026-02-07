64dd19dd5ed5da4c1c5a919c36387a09599005dd
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-02-07 09:18:48 +0530

    Update divisible or not by 5.c

--- divisible or not by 5.c
+++ divisible or not by 5.c
@@ -5,9 +5,10 @@ int main()
     int x;
     printf("Enter a number");
     scanf("%d",&x);
-    if(x%5)
-        printf("Not divisible by 5");
-    else
+    if(x%5==0)
         printf("divisible by 5");
+    else
+        printf("NOt divisible by 5");
     getch();
 }
+
