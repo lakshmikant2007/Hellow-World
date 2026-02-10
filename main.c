db1248d7704d51d221f7d5dfc337b27118c7d41c
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-02-10 20:33:34 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=1;
+    printf("Enter a number");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d\n",2*n+1-2*i);
+        i++;
+    }
+    return 0;
+}
