0d2608c7e127fcd507e6185ca290f2227c1f54f2
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-02-19 17:08:45 +0530

    Refactor TNRS.c for improved formatting

--- TNRS.c
+++ TNRS.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
-
-// Function Declaration
 int add();
-
 int main()
 {
     int s;
-
-    s = add();   // Function Call
-
+    s = add();   
     printf("Sum is %d", s);
-
     return 0;
 }
-
-// Function Definition
 int add()
 {
     int a, b, c;
-
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
-
     c = a + b;
-
-    return c;   // Returning value
+    return c;   
 }
+
