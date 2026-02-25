0fb2323baf0dd0c6877cbf8370e91b3c401e3ed1
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-02-25 20:58:09 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int f1(int n);
+
+int main()
+{
+    int k;
+    k=f1(3);
+    printf("%d",k);
+}
+int f1(int n)
+{
+    int s;
+    if(n==1)
+        return 1;
+    s=n+f1(n-1);
+    return (s);
+}
