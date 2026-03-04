aa50d9fd8a5502c45d4015a3905d9aca7156e6fa
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-03-04 18:45:16 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z') {
+        printf("It is an Uppercase letter");
+    }
+    else if (ch >= 'a' && ch <= 'z') {
+        printf("It is a Lowercase letter");
+    }
+    else {
+        printf("It is not an alphabet");
+    }
+
+    return 0;
+}
