df7c5755dcc67d7da50bb2ee92410dd84ab7ea12
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-03-08 12:35:23 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+//input 5 array and print it.
+#include<stdio.h>
+int main()
+{
+    int arr[5],i;
+    printf("Enter 5 numbers:\n");
+
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Array element are:\n");
+
+    for(i=0;i<5;i++)
+    {
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
