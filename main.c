d43473ec1b274cf81c0df273a5d0c041810eadb3
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-03-08 12:00:40 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[3], i, sum = 0;
+    float average;
+
+    printf("Enter three numbers:\n");
+
+    for(i=0; i<3; i++)
+    {
+        scanf("%d", &arr[i]);
+        sum = sum + arr[i];
+    }
+
+    average = sum / 3.0;
+
+    printf("Average = %.2f", average);
+
+    return 0;
+}
