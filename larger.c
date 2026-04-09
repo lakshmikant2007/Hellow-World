0cf0d58bcab972fe6286f034fd2ee0b0ea41b793
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-04-09 20:59:48 +0530

    Add files via upload

---
+++ larger.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+
+    printf("Enter two number:");
+    scanf("%d%d",&a,&b);
+
+    if(a>b)
+        printf("%d is larger",a);
+    else if(b>a)
+        printf("%d is larger",b);
+    else
+        printf("Both are equal");
+
+    return 0;
+}
