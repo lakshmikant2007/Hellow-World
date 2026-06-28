ccc203b48f0e5c02db7914ed8e18288d724b7ffc
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-06-28 22:06:59 +0530

    Add files via upload

---
+++ C.I.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float principal, rate, time;
+    float amount, compoundInterest;
+
+    printf("Enter Principal Amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter Rate of Interest: ");
+    scanf("%f", &rate);
+
+    printf("Enter Time (in years): ");
+    scanf("%f", &time);
+
+    amount = principal * pow((1 + rate / 100), time);
+
+    compoundInterest = amount - principal;
+
+    printf("Compound Interest = %.2f\n", compoundInterest);
+    printf("Total Amount = %.2f", amount);
+
+    return 0;
+}
